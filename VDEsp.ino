03e2efae5169b22a0b8c97d79b23f17af4a1dc36
Saikat Pinaki Banerjee <44677056+Saikat3Banerjee@users.noreply.github.com>
2019-02-04 10:57:59 -0500

    Add files via upload

---
+++ VDEsp.ino
@@ -0,0 +1,227 @@
+/***************************************************
+  Adafruit MQTT Library ESP8266 Example
+
+  Must use ESP8266 Arduino from:
+    https://github.com/esp8266/Arduino
+
+  Works great with Adafruit's Huzzah ESP board & Feather
+  ----> https://www.adafruit.com/product/2471
+  ----> https://www.adafruit.com/products/2821
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Tony DiCola for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+//int sensor = 0;
+  //int sensorValue = analogRead(A0);
+// const float BETA = 3650;
+//const float NOM_RESIST = 3999;
+//const float NOM_TEMP = 25;
+//const float R1 = 15;  // Fixed resistor next to thermistor. 0 if not required.
+//const float R2 = 200;  // Fixed resistor on other side of divider
+//const float Vcc = 3.300;  // Input voltage
+// Pin to measure from
+const float analogInPin = A0;
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       "UMBC Visitor"
+//#define WLAN_PASS       "...your password..."
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "saikat1"
+#define AIO_KEY         "af94a5d423344ea2bd30d139c65f39ab"
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+//WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'photocell' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish photocell = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/temp");
+
+// Setup a feed called 'onoff' for subscribing to changes.
+//Adafruit_MQTT_Subscribe onoffbutton = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/onoff");
+
+/*************************** Sketch Code ************************************/
+
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
+
+void setup() {
+
+   // Set up status LED
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+  delay(10);
+
+ //pinMode(sensor,INPUT);
+ 
+  Serial.println(F("Adafruit MQTT demo"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+  // Setup MQTT subscription for onoff feed.
+  //mqtt.subscribe(&onoffbutton);
+}
+http://www.jms-se.com/rtd.php
+// Converts resistance (in ohms) to temperature (in deg C)
+//float resistanceToCelsius(float resistance) {
+//  // Simplified Steinhart-Hart
+//  float temp = log(resistance / NOM_RESIST) / BETA;
+//  temp += 1.0 / (NOM_TEMP + 273.15);
+//  temp = (1 / temp) - 40;
+//  temp -= 273.15; // convert to C
+//  return temp;
+//}
+
+uint32_t x=0;
+
+void loop() {
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+
+  // this is our 'wait for incoming subscription packets' busy subloop
+  // try to spend your time here
+
+  // Now we can publish stuff!
+
+
+//void loop() {
+  // read the input on analog pin 0:
+//  float sum = 0;
+//  int iteration = 100;
+//  for( int i = 0; i < iteration; i++)
+//  {
+//      int sensorValue = (A0);
+//     Serial.print(sensorValue,6);
+//      sum += sensorValue;
+//        
+//  }
+//
+//   displayBusy();
+//   // convert the analog signal to voltage
+//  // the ESP2866 A0 reads between 0 and ~3 volts, producing a corresponding value
+//  // between 0 and 1024. The equation below will convert the value to a voltage value.
+//  analogVolts = (analogValue * 3.03) /1024;
+//  DebugLn(String(analogVolts));
+//  temperature = (analogVolts) * 100; //converting from 10 mv per degree wit 500 mV offset
+//                                                //to degrees ((voltage - 500mV) times 100)
+// 
+//   data = "temp_sensor=" + String(temperature);
+//  DebugLn(data);
+
+  // Serial.print(sensorValue,6);
+  //float voltage = sum * (5 / (1024*iteration));
+  //float temperature = ((voltage*1000) - 2877)/.97977;
+
+   digitalWrite(LED_BUILTIN, HIGH);
+  delay(10000);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  // Read the thermistor's voltage and thus get its resistance and temperature
+  float voltage = analogRead(analogInPin);
+  ;
+  //voltage= voltage / 680;
+  //voltage = (voltage / 1024);
+//  float totalResistance = ((Vcc * R2) / voltage) - R2;
+//  float resistance = (totalResistance - R1);
+//  float temp = resistanceToCelsius(resistance);
+voltage = voltage - 57.000;
+float temp = (3.502 * voltage) - 1619.50;
+
+   // Print status to serial console
+  Serial.print("Sensor voltage: ");
+  Serial.println(voltage);
+  //Serial.print("Resistance: ");
+  //Serial.println(resistance);
+  Serial.print("Temp: ");
+  Serial.println(temp);
+
+  
+  //Serial.println(voltage,6);//;Serial.print(" , ");
+  //Serial.println(temperature,4);
+
+    // Now we can publish stuff!
+  Serial.print(F("\nSending photocell val "));
+  Serial.print(x);
+  Serial.print("...");
+  //int temp_value = analogRead(A0);
+  if (! photocell.publish(temp)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+
+delay(100);
+
+  // ping the server to keep the mqtt connection alive
+  // NOT required if you are publishing once every KEEPALIVE seconds
+  /*
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+  */
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
